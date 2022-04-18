7182fbb6f5b5931fe53f6d7ad6b7c849efd92546
Kelly Walsh <kellywalsh827@gmail.com>
2022-04-18 11:56:23 -0600

    got questions to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const questions = [
   {
     q: "What are the kids names in Bob's Burgers?",
-    a: "b",
+    a: "Gene, Louise, Tina",
     choices: {
       a: "Frank, Katy, Marcie",
       b: "Gene, Louise, Tina",
@@ -82,6 +82,8 @@ const questions = [
   },
 ];
 
+var currentIndex = 0;
+
 var timeEl = document.querySelector(".timer");
 
 var secondsLeft = 60;
@@ -109,12 +111,64 @@ function sendMessage() {
 var startBtn = document.querySelector(".startBtn");
 
 startBtn.addEventListener("click", function () {
-  questions[0].q;
+  // questions[0].q;
   // console.log(questions[0].q);
-  questions[0].c[0];
+  // questions[0].c[0];
   // console.log(questions[0].c[0]);
   setTime();
+
+  var startContainer = document.querySelector(".startContainer");
+  startContainer.classList.add("hide");
+
+  displayQuestion()
 });
+
+function displayQuestion () {
+  var quizContainer = document.querySelector(".quizContainer");
+
+  quizContainer.innerHTML = "";
+
+  var questionH2 = document.createElement("h2");
+  questionH2.textContent = questions[currentIndex].q;
+
+  quizContainer.append(questionH2);
+  // var choicesH3a = document.createElement("h3");
+  // var choicesH3b = document.createElement("h3");
+  // var choicesH3c = document.createElement("h3");
+  // var choicesH3d = document.createElement("h3");
+  // choicesH3a.textContent = questions[0].choices.a;
+  // choicesH3b.textContent = questions[0].choices.b;
+  // choicesH3c.textContent = questions[0].choices.c;
+  // choicesH3d.textContent = questions[0].choices.d;
+
+  for(choice in questions[currentIndex].choices) {
+    var choicesBtn = document.createElement("button");
+    choicesBtn.textContent = questions[currentIndex].choices[choice];
+    choicesBtn.addEventListener("click", function (event) {
+      // get the value of the button
+      console.log(event.target.textContent)
+
+      // get the value of the correct answer
+      console.log(questions[currentIndex].a)
+
+      // compare
+      if(event.target.textContent === questions[currentIndex].a) {
+        alert("Correct!");
+      } else {
+        // take off 10 seconds
+        secondsLeft-=10
+        // secondsLeft = secondsLeft - 10
+      }
+
+      // move to the next question
+      currentIndex++;
+      displayQuestion()
+
+    })
+
+    quizContainer.append(choicesBtn)
+  }
+}
 
 function showQuestion(questions, quizContainer) {
   var output = [];
